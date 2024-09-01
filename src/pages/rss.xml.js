298d33513677f3b332a94c457279c8c7e4ce8227
Vincent S.-G. <hello@vinsg.ca>
2024-09-01 19:42:56 -0400

    add full content for rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,20 +1,24 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION, SOCIAL } from '../consts';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
-	const posts = await getCollection('posts', ({ data }) => {return data.draft !== true;}
-    );
+	const posts = await getCollection('posts', ({ data }) => { return data.draft !== true; }
+	);
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
 			title: post.data.title,
-      		pubDate: post.data.pubDate,
-      		description: post.data.description,
+			pubDate: post.data.pubDate,
+			description: post.data.description,
 			author: `${SOCIAL.EMAIL} (Vincent S.-G.)`,
 			link: `/blog/${post.slug}/`,
+			content: sanitizeHtml(parser.render(post.body)),
 		})),
 	});
 }
